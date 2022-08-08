35799848b7bbbce0a0937b010384d7d037a94338
Muhammad Shahnoor <shahnoorraza46@gmail.com>
2022-08-08 20:34:16 +0500

    Add Ajax for Search and Exception Handling

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -9,7 +9,7 @@ class Stock < ApplicationRecord
   #   )
   #   client.price(ticker)
   # end
-  def self.config_ief
+  def self.config_iex
     @client = IEX::Api::Client.new(
           publishable_token: Rails.application.credentials.iex_client[:sandbox_publishable_token],
           secret_token: Rails.application.credentials.iex_client[:sandbox_secret_token],
@@ -17,8 +17,12 @@ class Stock < ApplicationRecord
         )
   end
 
-  def self.get_price(ticker)
-    client = self.config_ief
-    client.price(ticker)
+  def self.get_iex_stock(ticker)
+    client = self.config_iex
+    begin
+      new(ticker: ticker, name: client.company(ticker).company_name, last_price: client.price(ticker))
+    rescue => exception
+      nil
+    end
   end
 end
